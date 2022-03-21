7e2f07a7ad3c85b2367cf932fb5470fe1583560d
Niraj <noisyking7@gmail.com>
2022-03-20 22:02:31 -0700

    added from clz

--- lab3/lab 3.2/app.js
+++ lab3/lab 3.2/app.js
@@ -1,13 +1,15 @@
-console.log('Welcome')
-
 const form = document.querySelector('.signup-form');
 const feedback = document.querySelector('.feedback');
+const feedback1 = document.querySelector('.feedback1');
 
 form.addEventListener('submit',e => {
     e.preventDefault();
 
     const username = form.username.value;
+    const number = form.number.value;
     const usernamePattern = /^[a-z A-Z]{6,12}$/;
+    const numberPattern = /^[0-9]{10}$/;
+
 
     if(usernamePattern.test(username)){
         feedback.textContent ='this username is valid';
@@ -15,6 +17,13 @@ form.addEventListener('submit',e => {
         else{
 
             feedback.textContent ='this username is not valid';
+        }
+   if(numberPattern.test(number)){
+        feedback1.textContent ='this number is valid';
+    }
+        else{
+
+            feedback1.textContent ='this number is not valid';
         }
 
 });
